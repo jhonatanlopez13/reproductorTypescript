4ecd542f5758e6ee6d74435f25190bd3ea83ed4d
jhonatan lopez <55928962+jhonatanlopez13@users.noreply.github.com>
2023-05-26 12:51:56 -0500

    Initial commit

---
+++ src/components/Busqueda.tsx
@@ -0,0 +1,9 @@
+import react from "React";
+
+export default function Busqueda() {
+  return (
+    <div>
+      <h1>Busqueda</h1>
+    </div>
+  );
+}
---
+++ src/components/ListaReproducion.tsx
@@ -0,0 +1,8 @@
+import react from "React";
+export default function ListaReproducion() {
+  return (
+    <div>
+      <h1>ListaReproducion</h1>
+    </div>
+  );
+}
